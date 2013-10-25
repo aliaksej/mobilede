051c86590d973bfd22db37da66733feba37fbf94
unknown <katarkevich@gmail.com>
2013-10-26 00:01:10 +0300

    Initial commit

---
+++ MobileDe.user.js
@@ -0,0 +1,19 @@
+﻿$('.pricePrimaryCountryOfSale.priceGross').click(function (e) {
+var price = e.target.innerText.replace(/[^\d]/g, '');
+var volMatches = $('.titleContainer').text().match(/\s+(\d[.,]\d)\s+/);
+var volEl = $('.technicalDetailsColumn dd:contains("см")');
+var engineVol;
+if (volEl.length > 0) {
+	engineVol = volEl.text().replace(/[^\d]/g, '');
+}
+else if (volMatches) {
+	engineVol = volMatches[1].replace(',', '.');
+}
+else {
+	engineVol = prompt('Enter engine volume', '1.6').replace(',', '.');
+}
+if (engineVol.indexOf('.')!== -1) {
+	engineVol = engineVol * 1000;
+}
+alert('Цена: ' + unsafeWindow.calculateTotalPrice(parseInt(price), parseInt(engineVol)));
+})
---
+++ calc.js
@@ -0,0 +1,37 @@
+var EUR2USD = 1.38;
+// converts eur 2 usd
+function eur2usd (eur) {
+  return eur * EUR2USD;
+}
+
+// get tax rate (EUR)
+function getTaxRate (engineVol) {
+  var tax = 1.5;
+  if (engineVol > 1000 && engineVol <= 1500) {
+    tax = 1.7;
+  }
+  else if (engineVol > 1500 && engineVol <= 1800) {
+    tax = 2.5;
+  }
+  else if (engineVol > 1800 && engineVol <= 2300) {
+    tax = 2.7;
+  }
+  else {
+    tax = 3;
+  }
+  return tax;
+}
+
+// carPrice (EUR), engineVol (cm3)
+function calculateTax (carPrice, engineVol) {
+  var tax = getTaxRate(engineVol);
+  var price = eur2usd(tax * engineVol);
+  return parseInt(price, 10);
+}
+// carPrice (EUR), engineVol (cm3)
+function calculateTotalPrice (carPrice, engineVol) {
+  var tax = getTaxRate(engineVol);
+  var price= eur2usd(carPrice + tax * engineVol);
+  return parseInt(price, 10);
+}
+window.calculateTotalPrice = calculateTotalPrice;
